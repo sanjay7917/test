5aae84b7404775b527c1f632f0a737ce6d86c8b5
mohit <mohitk972@gmail.com>
2023-05-19 16:37:02 +0530

    changes made

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,7 +2,16 @@ def call(Map stageParams) {
 
     checkout([
         $class: 'GitSCM'
-        branches: [[name: stageParams.branch]]
-        userRemoteConfigs: [[url: stageParams.url]]
+        branches: [[name: stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
     ])
 }
+
+// def call(Map stageParams) {
+ 
+//     checkout([
+//         $class: 'GitSCM',
+//         branches: [[name:  stageParams.branch ]],
+//         userRemoteConfigs: [[ url: stageParams.url ]]
+//     ])
+//   }
