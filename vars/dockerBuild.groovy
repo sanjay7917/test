50b4824ba82141dba75ae82b4f3cf3301315ccfc
mohit <mohitk972@gmail.com>
2023-05-20 16:09:21 +0530

    new file added

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -4,5 +4,5 @@ def call(String project, String ImageTag, String hubUser){
      docker image build -t ${hubUser}/${project} . 
      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
-    """
+    """  
 }
